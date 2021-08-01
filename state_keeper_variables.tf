2acdfa2e1d794428025396a141c78791d2f2b226
lividlair <lividlair@Ryans-MacBook-Pro.local>
2021-08-01 12:53:16 -0400

    fmt files

--- state_keeper_variables.tf
+++ state_keeper_variables.tf
@@ -1,9 +1,9 @@
-variable bucket_name {
+variable "bucket_name" {
   type        = string
   description = "The name of the bucket for storing terraform state"
 }
 
-variable dynamodb_table_name {
+variable "dynamodb_table_name" {
   type        = string
   description = "The name of the dynamodb table that handles locking"
 }
